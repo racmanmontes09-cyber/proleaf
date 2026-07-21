5e347cf1a14ba0ac9bbc6967d412bfc94e607d45
RacmanMontes <racmanmontes@gmail.com>
2026-07-21 20:06:25 +0800

    faah

--- app/Livewire/Dashboard/DeviceStatus.php
+++ app/Livewire/Dashboard/DeviceStatus.php
@@ -3,13 +3,14 @@
 namespace App\Livewire\Dashboard;
 
 use Livewire\Component;
+use App\Models\Device;
 
 class DeviceStatus extends Component
 {
-   public function render()
-{
-    return view('livewire.dashboard.device-status', [
-        'device' => Device::first(),
-    ]);
-}
+    public function render()
+    {
+        return view('livewire.dashboard.device-status', [
+            'device' => Device::first(),
+        ]);
+    }
 }
